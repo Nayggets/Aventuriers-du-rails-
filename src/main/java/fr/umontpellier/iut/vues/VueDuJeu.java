4f40fe32284d564d717517a8f4664628b542c523
Melotte Quentin <quentin.melotte@etu.umontpellier.fr>
2022-05-26 17:23:56 +0000

    Ajoute cartes Visibles

--- src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
@@ -1,13 +1,22 @@
 package fr.umontpellier.iut.vues;
 
+import fr.umontpellier.iut.ICouleurWagon;
 import fr.umontpellier.iut.IDestination;
 import fr.umontpellier.iut.IJeu;
+import fr.umontpellier.iut.rails.CouleurWagon;
 import fr.umontpellier.iut.rails.Destination;
+import fr.umontpellier.iut.rails.Jeu;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -25,62 +34,136 @@ public class VueDuJeu extends VBox {
     private IJeu jeu;
     private VuePlateau plateau;
     private  Button passer;
-    private VBox listeDestinations;
+    private HBox listeDestinations;
+    private HBox cartesVisibles;
     private VueJoueurCourant vueJCour;
+
+    private VuePlateau vuePlateau;
+
+    private VBox joueur;
 
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
+
+        //jeu.joueurCourantProperty().get().cartesWagonProperty().add(CouleurWagon.JAUNE);
         plateau = new VuePlateau();
-        listeDestinations = new VBox();
+        listeDestinations = new HBox();
+        listeDestinations.setSpacing(50);
+        cartesVisibles = new HBox();
+        cartesVisibles.setSpacing(60);
+        vuePlateau = new VuePlateau();
+        joueur = new VBox();
+
         passer = new Button("Passer");
         vueJCour = new VueJoueurCourant(jeu);
 
+
+
+        getChildren().add(vuePlateau);
         getChildren().add(listeDestinations);
         getChildren().add(passer);
+        getChildren().add(cartesVisibles);
         getChildren().add(vueJCour);
-
-
-        getChildren().add(plateau);
     }
 
     public IJeu getJeu() {
         return jeu;
     }
 
-    public Label trouveLabelDestination(IDestination id){
+    public VueDestination trouveVuDestination(IDestination id){
         for (int i = 0; i < listeDestinations.getChildren().size() ; i++) {
-            Label label =  (Label) listeDestinations.getChildren().get(i);
-            if (label.getText().equals(id.getNom())){
-                return label;
+            VueDestination destination =  (VueDestination) listeDestinations.getChildren().get(i);
+            if (id.getNom().equals(id.getNom())){
+                return destination;
+            }
+        }
+        return null;
+    }
+
+    public VueCarteWagon trouveCarteVagon(ICouleurWagon id){
+        for (int i = 0; i < cartesVisibles.getChildren().size() ; i++) {
+            VueCarteWagon vueWagon =  (VueCarteWagon) cartesVisibles.getChildren().get(i);
+            if (vueWagon.toString().equals(id.toString())){
+                return vueWagon;
             }
         }
         return null;
     }
 
     public void creerBindings() {
-        getJeu().destinationsInitialesProperty().addListener(listener);
-        vueJCour.
-                creerBindings();
+        ObservableList<Node> liste = listeDestinations.getChildren();
+        for(Node e : liste){
+            VueDestination b = (VueDestination) e;
+            b.setOnAction(actionEvent -> {
 
+                    System.out.println("wesh");
+                    jeu.uneDestinationAEteChoisie(b.getDestination().getNom());
+                    listeDestinations.getChildren().remove(b);
+            });
+        }
+        getJeu().destinationsInitialesProperty().addListener(listener);
+
+        getJeu().cartesWagonVisiblesProperty().addListener(listenWagon);
         passer.setOnAction(actionEvent -> {
             getJeu().passerAEteChoisi();
         });
+        vueJCour.creerBindings();
 
     }
 
-    ListChangeListener<Destination> listener = new ListChangeListener<Destination>() {
+    private ListChangeListener<IDestination> listener = new ListChangeListener<IDestination>() {
         @Override
-        public void onChanged(Change<? extends Destination> change ) {
+        public void onChanged(Change<? extends IDestination> change ) {
             Platform.runLater(() -> {
                 while (change.next()) {
                     if (change.wasAdded()) {
                         for (int i = 0; i < change.getAddedSize(); i++) {
-                            listeDestinations.getChildren().add(new Label(change.getAddedSubList().get(i).getNom()));
+                            VueDestination vue = new VueDestination(change.getAddedSubList().get(i));
+                            vue.setOnAction(actionEvent -> {
+
+                                System.out.println("wesh");
+                                jeu.uneDestinationAEteChoisie(vue.getDestination().getNom());
+                                listeDestinations.getChildren().remove(vue);
+                            });
+                            listeDestinations.getChildren().add(vue);
+
                         }
                     }
                     if(change.wasRemoved()){
                         for (int i = 0; i < change.getRemovedSize(); i++) {
-                            listeDestinations.getChildren().remove(trouveLabelDestination(change.getRemoved().get(i)));
+                            listeDestinations.getChildren().remove(trouveVuDestination(change.getRemoved().get(i)));
+                        }
+                    }
+                    if(change.wasUpdated()){
+
+                    }
+                }
+            });
+        }
+
+    };
+
+    private ListChangeListener<ICouleurWagon> listenWagon = new ListChangeListener<ICouleurWagon>() {
+        @Override
+        public void onChanged(Change<? extends ICouleurWagon> change ) {
+            Platform.runLater(() -> {
+                while (change.next()) {
+                    if (change.wasAdded()) {
+                        for (int i = 0; i < change.getAddedSize(); i++) {
+                            VueCarteWagon vue = new VueCarteWagon(change.getAddedSubList().get(i),false);
+                            vue.setOnAction(actionEvent -> {
+
+                                System.out.println("wesh");
+                                jeu.uneCarteWagonAEteChoisie(vue.getCouleurWagon());
+                                cartesVisibles.getChildren().remove(vue);
+                            });
+
+                            cartesVisibles.getChildren().add(vue);
+                        }
+                    }
+                    if(change.wasRemoved()){
+                        for (int i = 0; i < change.getRemovedSize(); i++) {
+                            cartesVisibles.getChildren().remove(trouveCarteVagon(change.getRemoved().get(i)));
                         }
                     }
                 }
