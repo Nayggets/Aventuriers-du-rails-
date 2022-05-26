fc477506ac24b04f99179600645b406b89f9b220
Melotte Quentin <quentin.melotte@etu.umontpellier.fr>
2022-05-26 17:24:19 +0000

    Update VueCarteWagon.java

--- src/main/java/fr/umontpellier/iut/vues/VueCarteWagon.java
+++ src/main/java/fr/umontpellier/iut/vues/VueCarteWagon.java
@@ -1,23 +1,53 @@
 package fr.umontpellier.iut.vues;
 
 import fr.umontpellier.iut.ICouleurWagon;
-import fr.umontpellier.iut.IJeu;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 /**
  * Cette classe représente la vue d'une carte Wagon.
  *
  * On y définit le listener à exécuter lorsque cette carte a été choisie par l'utilisateur
  */
-public class VueCarteWagon extends Pane {
+public class VueCarteWagon extends Button {
 
     private ICouleurWagon couleurWagon;
+    private ImageView view;
 
-    public VueCarteWagon(ICouleurWagon couleurWagon) {
+    public VueCarteWagon(ICouleurWagon couleurWagon,boolean carteJoueur) {
         this.couleurWagon = couleurWagon;
+
+        String str = couleurWagon.toString().toLowerCase();
+        System.out.println(str);
+        view = new ImageView("/images/cartesWagons/carte-wagon-" + couleurWagon.toString().toUpperCase() + ".png");
+        if(carteJoueur){
+            view.setFitHeight(100);
+            view.setFitWidth(125);
+
+
+
+            this.setRotate(90);
+        }
+        else{
+            view.setFitHeight(125);
+            view.setFitWidth(150);
+        }
+
+        this.setGraphic(view);
+
     }
+
+    public void hoverMode(double height, double width,double y){
+        view.setFitWidth(width);
+        view.setFitHeight(height);
+
+        this.setTranslateY(y);
+    }
+
+
 
     public ICouleurWagon getCouleurWagon() {
         return couleurWagon;
     }
+
 }
