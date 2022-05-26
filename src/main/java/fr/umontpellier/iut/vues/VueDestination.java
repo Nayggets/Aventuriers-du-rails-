a690eddc780acb891cac585a0e43dccc361717f1
Melotte Quentin <quentin.melotte@etu.umontpellier.fr>
2022-05-26 17:23:07 +0000

    Update VueDestination.java

--- src/main/java/fr/umontpellier/iut/vues/VueDestination.java
+++ src/main/java/fr/umontpellier/iut/vues/VueDestination.java
@@ -1,6 +1,9 @@
 package fr.umontpellier.iut.vues;
 
 import fr.umontpellier.iut.IDestination;
+import fr.umontpellier.iut.rails.Destination;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 /**
@@ -8,13 +11,34 @@ import javafx.scene.layout.Pane;
  *
  * On y définit le listener à exécuter lorsque cette carte a été choisie par l'utilisateur
  */
-public class VueDestination extends Pane {
+public class VueDestination extends Button {
 
     private IDestination destination;
 
     public VueDestination(IDestination destination) {
         this.destination = destination;
+        String str = "/images/missions/eu-" + destination.toString().toLowerCase() + ".png";
+        str = str.replace("(","");
+        str = str.replace(")","");
+        int i = 0;
+        while(i < str.length()){
+            if(str.charAt(i) >= '0' && str.charAt(i) <= '9'){
+                str = str.replace(str.charAt(i),' ');
+            }
+            i++;
+        }
+        str = str.replaceAll(" ","");
+        System.out.println(str);
+        ImageView view = new ImageView(str);
+        view.setFitWidth(150);
+        view.setFitHeight(120);
+        this.setGraphic(view);
+
     }
+
+
+
+
 
     public IDestination getDestination() {
         return destination;
