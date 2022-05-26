f7a4cfb82b1f7655cdb4ee792427ac85d02939d9
Melotte Quentin <quentin.melotte@etu.umontpellier.fr>
2022-05-26 17:22:32 +0000

    Update VueJoueurCourant.java

--- src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
+++ src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
@@ -8,8 +8,11 @@ import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -21,22 +24,24 @@ import javafx.scene.layout.VBox;
 public class VueJoueurCourant extends VBox {
     private Label nomJoueur;
     private IJeu jeu;
-    private VBox cartesJoueurCourant;
+    private HBox cartesJoueurCourant;
     private HBox listeCarteVisible;
-    private Label cartes;
-    private VueCarteWagon cartesImg;
+
     private HBox cartesJoueur;
 
 
     public VueJoueurCourant(IJeu jeu){
         this.jeu = jeu;
         this.nomJoueur = new Label(jeu.getJoueurs().get(0).getNom());
-        this.cartes = new Label(jeu.getJoueurs().get(0).getCartesWagon().toString());
-        this.cartesJoueurCourant = new VBox();
+        this.cartesJoueurCourant = new HBox();
+        cartesJoueurCourant.alignmentProperty().set(Pos.BASELINE_CENTER);
+        cartesJoueurCourant.setSpacing(-75);
+        cartesJoueurCourant.setPadding(new Insets(0,0,0,0));
+        cartesJoueurCourant.setTranslateY(65);
         this.listeCarteVisible = new HBox();
 
-        getChildren().add(listeCarteVisible);
-        cartesJoueurCourant.getChildren().add(cartes);
+
+
         getChildren().add(nomJoueur);
         getChildren().add(cartesJoueurCourant);
     }
@@ -44,7 +49,7 @@ public class VueJoueurCourant extends VBox {
     public void creerBindings(){
         jeu.joueurCourantProperty().addListener(listener);
         //jeu.getJoueurs().get(0).cartesWagonProperty().addListener(listener2);
-        jeu.joueurCourantProperty().get().cartesWagonProperty().addListener(listener2);
+        //jeu.joueurCourantProperty().get().cartesWagonProperty().addListener(listener2);
         //jeu.cartesWagonVisiblesProperty().addListener(listener3);
 
     }
@@ -52,18 +57,41 @@ public class VueJoueurCourant extends VBox {
     //nomJoueur.setText(jeu.joueurCourantProperty().get().getNom());
 
     //joueur courant affichage nom + liste cartes
-    ChangeListener<IJoueur> listener = new ChangeListener<IJoueur>() {
+    private ChangeListener<IJoueur> listener = new ChangeListener<IJoueur>() {
         @Override
         public void changed(ObservableValue<? extends IJoueur> observableValue, IJoueur iJoueur, IJoueur t1) {
             Platform.runLater(() -> {
                 nomJoueur.setText(t1.getNom());
-                cartes.setText(t1.getCartesWagon().toString());
+                cartesJoueurCourant.getChildren().clear();
+                for(CouleurWagon c : t1.getCartesWagon()){
+                    VueCarteWagon vue = new VueCarteWagon(c,true);
+                    vue.rotateProperty().add(90);
+                    vue.setOnMouseEntered(actionEvent -> {
+                        vue.hoverMode(150,175,-75);
+
+                    });
+                    vue.setOnMouseExited(actionEvent ->{
+                        vue.hoverMode(100,125,0);
+                    });
+                    cartesJoueurCourant.getChildren().add(vue);
+                }
             });
         }
     };
 
+    public VueCarteWagon trouveCarteVagon(ICouleurWagon id){
+        for (int i = 0; i < cartesJoueurCourant.getChildren().size() ; i++) {
+            VueCarteWagon vueWagon =  (VueCarteWagon) cartesJoueurCourant.getChildren().get(i);
+            if (vueWagon.toString().equals(id.toString())){
+                return vueWagon;
+            }
+        }
+        return null;
+    }
+
+
     //liste carte joueur
-    ListChangeListener<CouleurWagon> listener2 = new ListChangeListener<CouleurWagon>() {
+    private ListChangeListener<CouleurWagon> listener2 = new ListChangeListener<CouleurWagon>() {
         @Override
         public void onChanged(Change<? extends CouleurWagon> change) {
             Platform.runLater(() -> {
@@ -75,3 +103,4 @@ public class VueJoueurCourant extends VBox {
         }
     };
 }
+
