67f2ef11a76fa82a157fa045672d1095a07809fe
Chevallier Julie <julie.chevallier@etu.umontpellier.fr>
2022-06-11 14:17:35 +0200

    Visuel DebutJeu

--- src/main/java/fr/umontpellier/iut/vues/DebutDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/DebutDuJeu.java
@@ -1,5 +1,6 @@
 package fr.umontpellier.iut.vues;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -19,17 +20,18 @@ public class DebutDuJeu extends Stage {
     private Label miniRule;
 
     public DebutDuJeu(){
+
         miniRule = new Label("2-5 joueur \n30-60\n8 ans et plus");
-        miniRule.setAlignment(Pos.BOTTOM_LEFT);
         aventurier = new ImageView("/images/titre.png");
-
-
         Image view = new Image("/images/imgAcceuil.png");
         rule = new Button();
+        rule.setStyle("-fx-background-color: transparent;");
         rule.setGraphic(new ImageView("/images/option.png"));
         play = new Button();
+        play.setStyle("-fx-background-color: transparent;");
         play.setGraphic(new ImageView("/images/play.png"));
         exit = new Button();
+        exit.setStyle("-fx-background-color: transparent;");
         exit.setGraphic(new ImageView("/images/exit.png"));
         vBox = new VBox();
         play.setOnAction(actionEvent -> {
@@ -39,20 +41,37 @@ public class DebutDuJeu extends Stage {
             exited = true;
             this.close();
         });
-
         vBox.setBackground(new Background(new BackgroundImage(view, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO,true,true,true,true))));
+
         Scene scene = new Scene(vBox);
         this.setMaximized(true);
         this.setFullScreen(true);
-
-
-        rule.setAlignment(Pos.TOP_RIGHT);
-        play.setAlignment(Pos.CENTER);
-        exit.setAlignment(Pos.BOTTOM_RIGHT);
-        vBox.getChildren().addAll(rule,aventurier,play,exit,miniRule);
-        vBox.setAlignment(Pos.CENTER);
         this.setScene(scene);
 
+
+        //---partie visuelle---
+        BorderPane root = new BorderPane();
+        play.setPadding(new Insets(100,0,0,0));
+        VBox v = new VBox(aventurier,play);
+        v.setAlignment(Pos.CENTER);
+        v.setPadding(new Insets(0,0,0,100));
+        root.setCenter(v);
+
+        BorderPane bottom = new BorderPane();
+        HBox exith = new HBox(exit);
+        exith.setPadding(new Insets(75,0,0,0));
+        bottom.setRight(exith);
+        miniRule.setStyle("-fx-font-size: 40;-fx-text-fill: white; -fx-font-weight: bold");
+        bottom.setLeft(miniRule);
+        bottom.setPadding(new Insets(275,0,0,0));
+        //top droite bottom gauche
+        root.setBottom(bottom);
+
+        BorderPane top = new BorderPane();
+        top.setRight(rule);
+        root.setTop(top);
+
+        vBox.getChildren().addAll(root);
     }
 
     public boolean isExited(){
