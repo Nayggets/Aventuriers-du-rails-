9e3503019981e702623e7abdc8e7179e80f4680c
Melotte Quentin <quentin.melotte@etu.umontpellier.fr>
2022-06-12 08:27:32 +0000

    Debut du jeu modification

--- src/main/java/fr/umontpellier/iut/vues/DebutDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/DebutDuJeu.java
@@ -12,21 +12,23 @@ import javafx.stage.Stage;
 
 public class DebutDuJeu extends Stage {
     private VBox vBox;
-    private boolean exited = false;
+    private boolean launch = false;
     private Button rule;
     private Button play;
     private Button exit;
+    private Button left;
+    private Button right;
     private ImageView aventurier;
+    private ArrayList<ImageView> regle;
+    private int nbRule;
     private Label miniRule;
-
-
+    private HBox regleButton;
+    private HBox menu;
 
     /////////
 
-    private Button next;
-    private Button back;
-
     public DebutDuJeu(){
+        regle = new ArrayList<>();
         miniRule = new Label("2-5 joueur \n30-60\n8 ans et plus");
         aventurier = new ImageView("/images/titre.png");
         Image view = new Image("/images/imgAcceuil.png");
@@ -40,6 +42,66 @@ public class DebutDuJeu extends Stage {
         exit.setStyle("-fx-background-color: transparent;");
         exit.setGraphic(new ImageView("/images/exit.png"));
         vBox = new VBox();
+
+        ImageView view0 = new ImageView("/images/canvas.png");
+        view0.setFitHeight(1200);
+        view0.setFitWidth(1200);
+        regle.add(view0);
+        view0 = new ImageView("/images/canvas1.png");
+        view0.setFitHeight(1200);
+        view0.setFitWidth(1200);
+
+        regle.add(view0);
+        view0 = new ImageView("/images/canvas2.png");
+        view0.setFitHeight(1200);
+        view0.setFitWidth(1200);
+        regle.add(view0);
+        view0 = new ImageView("/images/canvas3.png");
+        view0.setFitHeight(1200);
+        view0.setFitWidth(1200);
+        regle.add(view0);
+        view0 = new ImageView("/images/canvas4.png");
+        view0.setFitHeight(1200);
+        view0.setFitWidth(1200);
+        regle.add(view0);
+        view0 = new ImageView("/images/canvas5.png");
+        view0.setFitHeight(1200);
+        view0.setFitWidth(1200);
+        regle.add(view0);
+        regleButton = new HBox();
+        regleButton.setSpacing(0);
+        left = new Button();
+        left.setOnAction(actionEvent -> {
+            if(nbRule > 0){
+                nbRule--;
+                regleButton.getChildren().set(1,regle.get(nbRule));
+            }
+            else{
+                menu.getChildren().set(0,vBox);
+            }
+
+        });
+        left.setGraphic(new ImageView("/images/Left.png"));
+        left.setMinHeight(200);
+        regleButton.getChildren().add(left);
+        regleButton.getChildren().add(view0);
+
+        right = new Button();
+        right.setOnAction(actionEvent -> {
+            if(nbRule < 5){
+                nbRule++;
+                regleButton.getChildren().set(1,regle.get(nbRule));
+            }
+            else{
+                menu.getChildren().set(0,vBox);
+
+            }
+
+
+        });
+        right.setMinHeight(200);
+        right.setGraphic(new ImageView("/images/Right.png"));
+        regleButton.getChildren().add(right);
         play.setOnAction(actionEvent -> {
             this.close();
         });
@@ -47,7 +109,8 @@ public class DebutDuJeu extends Stage {
             exited = true;
             this.close();
         });
-        vBox.setBackground(new Background(new BackgroundImage(view, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO,true,true,true,true))));
+        menu = new HBox();
+        menu.setBackground(new Background(new BackgroundImage(view, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO,true,true,true,true))));
 
         Scene scene = new Scene(vBox);
         this.setMaximized(true);
